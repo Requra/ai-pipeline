e33198f3d04b548a8799851fd61bba962a4553ce
carolehab-dev <carolehab11@gmail.com>
2026-05-15 22:38:46 +0300

    feat(fix): Create generate node for user stories + Update classify node for multi-labeled

--- ai-service/app/llm.py
+++ ai-service/app/llm.py
@@ -20,9 +20,9 @@ def get_llm(model_name: str = "gemini-2.0-flash"):
             temperature=0,
             openai_api_key=os.getenv("OPENAI_API_KEY")
         )
-    elif model_name == "gpt-oss-20b":
+    elif model_name == "gpt-oss-120b":
         return ChatOpenAI(
-            model="openai/gpt-oss-20b:free",
+            model="openai/gpt-oss-120b:free",
             temperature=0,
             api_key=os.getenv("GPT_OSS_API_KEY"),
             base_url=os.getenv("BASE_URL_KEY")
--- ai-service/app/schemas/items.py
+++ ai-service/app/schemas/items.py
@@ -9,7 +9,8 @@ class FunctionalRequirement(BaseModel):
     source_hint: str
 
 class ClassifiedRequirement(FunctionalRequirement):
-    label: Literal["FR", "NFR", "BR"]
+    # label: Literal["FR", "NFR", "BR"]
+    labels: List[Literal["FR", "NFR", "BR"]]
     confidence: float
 
 class AcceptanceCriterion(BaseModel):
@@ -21,7 +22,8 @@ class UserStory(BaseModel):
     description: str
     acceptance_criteria: List[AcceptanceCriterion]
     source_fr_id: int
-    label: Literal["FR", "NFR", "BR"]
+    # label: Literal["FR", "NFR", "BR"]
+    labels: List[Literal["FR", "NFR", "BR"]]
 
 class JobResult(BaseModel):
     job_id: str
